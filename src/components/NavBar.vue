ebd7d81825aecaab992a47cdcce72f3b308ec282
Anastacyy <bodnar.anastasiia.2007@gmail.com>
2025-12-21 22:13:28 +0200

    feat: add navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,30 @@
+<script setup>
+</script>
+
+<template>
+  <nav class=" text-white py-4 px-8 flex justify-between items-center absolute w-full top-0 z-50">
+    
+    <div class="text-2xl font-bold ">
+      Just<span class="text-primary">Stay</span>
+    </div>
+
+    <ul class="hidden md:flex gap-8 font-medium">
+      <li><a href="#home" class="hover:text-primary transition">Home</a></li>
+      <li><a href="#about" class="hover:text-primary transition">About</a></li>
+      <li><a href="#services" class="hover:text-primary transition">Services</a></li>
+      <li><a href="#rooms" class="hover:text-primary transition">Rooms</a></li>
+      <li><a href="#contact" class="hover:text-primary transition">Contact</a></li>
+    </ul>
+
+    <div class="flex items-center gap-2 font-medium text-lg">
+      <span class="text-primary font-bold cursor-default">EN</span>
+      
+      <span class="text-gray-300">|</span>
+      
+      <a href="/" class="text-gray-400 hover:text-gray-800 transition cursor-pointer" title="Switch to Ukrainian">
+        UA
+      </a>
+    </div>
+
+  </nav>
+</template>
