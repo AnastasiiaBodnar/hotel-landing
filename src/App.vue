8ca21226e2cac3d009a60b3dfc2bd824c2f857f5
Anastacyy <bodnar.anastasiia.2007@gmail.com>
2026-01-02 23:37:28 +0200

    feat: add initial structure rooms section

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 import NavBar from './components/NavBar.vue'
 import HeroSection from './components/HeroSection.vue';
 import AboutSection from './components/AboutSection.vue';
+import RoomsSection from './components/RoomsSection.vue';
 </script>
 
 <template>
     <NavBar />
     <HeroSection />
     <AboutSection/>
+    <RoomsSection/>
 </template>
---
+++ src/components/RoomsSection.vue
@@ -0,0 +1,37 @@
+<script setup>
+import aboutImage from '../assets/about.png'
+</script>
+
+<template>
+    <section id="rooms" class="pt-5 bg-background" >
+        <div>
+            <p>
+                — Our Rooms — 
+            </p>
+
+            <h2>
+                Explore Our Rooms
+            </h2>
+        </div>
+
+        <div>
+            <div>
+                <img :src="aboutImage" alt="Junior Suite">
+                <h3>Junior Suite</h3>
+                <p>$150.00 / per night</p>
+            </div>
+
+             <div>
+                <img :src="aboutImage" alt="Executive Suite">
+                <h3>Executive Suite</h3>
+                <p>$250.00 / per night</p>
+            </div>
+
+             <div>
+                <img :src="aboutImage" alt="Super Deluxe">
+                <h3>Super Deluxe</h3>
+                <p>$350.00 / per night</p>
+            </div>
+        </div>
+    </section>
+</template>
