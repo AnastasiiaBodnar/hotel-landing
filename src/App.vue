39c48ce401d357b3c7a90868af6edef4b38c4ccf
Anastacyy <bodnar.anastasiia.2007@gmail.com>
2026-01-12 23:57:14 +0200

    feat: add initial services section structure

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import NavBar from './components/NavBar.vue'
 import HeroSection from './components/HeroSection.vue';
 import AboutSection from './components/AboutSection.vue';
 import RoomsSection from './components/RoomsSection.vue';
+import ServicesSection from './components/ServicesSection.vue';
 </script>
 
 <template>
@@ -10,4 +11,5 @@ import RoomsSection from './components/RoomsSection.vue';
     <HeroSection />
     <AboutSection/>
     <RoomsSection/>
+    <ServicesSection/>
 </template>
---
+++ src/components/ServicesSection.vue
@@ -0,0 +1,56 @@
+<script setup>
+</script>
+
+<template>
+    <section id="services" class="pt-8">
+        <div>
+            <p>
+                — Our Services —
+            </p>
+        </div>
+
+        <div>
+            <h2>
+                Explore Our <span class="text-primary">Services</span>
+            </h2>
+        </div>
+
+        <div>
+            <div>
+                <span>[Icon]</span>
+                <h3>Rooms & Appartment</h3>
+                <p>Figma ipsum component variant main layer.</p>
+            </div>
+
+            <div>
+                <span>[Icon]</span>
+                <h3>Sports & Gaming</h3>
+                <p>Figma ipsum component variant main layer.</p>
+            </div>
+
+            <div>
+                <span>[Icon]</span>
+                <h3>Food & Restaurant</h3>
+                <p>Figma ipsum component variant main layer.</p>
+            </div>
+
+            <div>
+                <span>[Icon]</span>
+                <h3>Spa & Fitness</h3>
+                <p>Figma ipsum component variant main layer.</p>
+            </div>
+
+            <div>
+                <span>[Icon]</span>
+                <h3>Event & Party</h3>
+                <p>Figma ipsum component variant main layer.</p>
+            </div>
+
+            <div>
+                <span>[Icon]</span>
+                <h3>GYM & Yoga</h3>
+                <p>Figma ipsum component variant main layer.</p>
+            </div>
+        </div>
+    </section>
+</template>
